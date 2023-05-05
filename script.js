11451fea2570a6e216af80baa427650c646c6e51
keebot-png <kee.bot@icloud.com>
2023-05-05 23:15:59 +0200

    FInish styling cards, fix grammar and linters.

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const popData = [
     Project_name: 'Meals API',
     Subtitle: 'JavaScript meals API project',
     Technologies: ['CSS', 'Javascript', 'HTML'],
-    Description: 'This project is build around the MealDB API using HTML5, CSS and JavaScript. You can view various types of Dishes & Meals Moreover, you can interact with the meals list by giving them a like or by adding some comments about them.',
+    Description: 'This project is built around the MealDB API using HTML5, CSS, and JavaScript. You can view various types of dishes and meals, and moreover, you can interact with the meal list by giving them a like or adding some comments about them.',
     thumbnail: './Images/card_images/restuarant-meal.png',
     ProjectLink: 'https://graceabike.github.io/JavaScript-Capstone-Project/dist/index.html',
     GitHubLink: 'https://github.com/keebot-png/JavaScript-Capstone-Project',
@@ -38,7 +38,7 @@ const popData = [
     Project_name: 'React calculator',
     Subtitle: 'Math calculator build with React',
     Technologies: ['CSS', 'Javascript', 'React', 'Redux'],
-    Description: 'This web app lets users make simple calculations and also allows user to see random quotes right from the application, built with React following SPA approach.',
+    Description: "This web app lets users make simple calculations and also allows the user to see random quotes right from the application. It's built with React following a Single Page Application (SPA) approach.",
     thumbnail: './Images/card_images/react-calculator.png',
     ProjectLink: 'https://maths-calculator-quote.netlify.app/',
     GitHubLink: 'https://github.com/keebot-png/math-magicians-1',
@@ -47,7 +47,7 @@ const popData = [
     Project_name: 'Tech conference website',
     Subtitle: 'Conference website to promote a tech event',
     Technologies: ['Bootstrap', 'CSS', 'Javascript', 'HTML'],
-    Description: 'This is the first capstone project of the Microverse course which involved promoting an online tech event which showcased the latest technologies. This website encompasses everything that we learned in the first module and it was built using Bootstrap.',
+    Description: 'This is the first capstone project of the Microverse course, which involves promoting an online tech event showcasing the latest technologies. This website encompasses everything that we learned in the first module and was built using Bootstrap.',
     thumbnail: './Images/card_images/tech-conference.png',
     ProjectLink: 'https://keebot-png.github.io/Capstone-project-1/',
     GitHubLink: 'https://github.com/keebot-png/Capstone-project-1',
@@ -56,7 +56,7 @@ const popData = [
     Project_name: 'Space travel project',
     Subtitle: 'Space Travel project with React and Redux',
     Technologies: ['React', 'Redux', 'Javascript', 'Bootstrap'],
-    Description: 'A website that uses a real live api from space x and displays it, You are able to Reserve a rocket and join missions which displays on your profile page.',
+    Description: 'This website uses a real-life API from SpaceX and displays it. You are able to reserve a rocket and join missions which are displayed on your profile page.',
     thumbnail: './Images/card_images/falcon-app.png',
     ProjectLink: 'https://space-travel-site-react.netlify.app/',
     GitHubLink: 'https://github.com/keebot-png/Space-Travel-Project',
@@ -65,7 +65,7 @@ const popData = [
     Project_name: 'Ecommerce style React and Redux',
     Subtitle: 'Keeping track of \nhundreds of components',
     Technologies: ['React', 'CSS', 'Bootstrap', 'Redux'],
-    Description: 'This is a mobile app that is built using React and Redux as a state management system. This is an ecommerce store that displays product categories and the details of those specific products e.g. size, brand, color etc. IMPORTANT: This has only been designed for MOBILE use ONLY. I am currently modifying it to suit desktop as well.',
+    Description: "This is a mobile app built using React and Redux as a state management system. It's an ecommerce store that displays product categories and the details of those specific products (e.g., size, brand, color, etc.). IMPORTANT: This app has only been designed for MOBILE use ONLY. I am currently modifying it to suit desktop as well.",
     thumbnail: './Images/card_images/shopping-site.png',
     ProjectLink: 'https://simple-shopping-app-react-redux.netlify.app/',
     GitHubLink: 'https://github.com/keebot-png/Shopping-Website',
@@ -74,7 +74,7 @@ const popData = [
     Project_name: 'Awesome Books JavaScript',
     Subtitle: 'Awesome Books JavaScript',
     Technologies: ['CSS', 'Javascript', 'HTML', 'Bootstrap'],
-    Description: 'A basic website that allows you to add and remove books. There has been added styling to match the provided template using Bootstrap. Classes and methods were used in this project instead of objects and arrays. A functional Navbar, footer and a contact page was created. Each nav-item contains an onclick event listener containing a function which enables the user to switch to the corresponding page. Lastly the timestamp below the navbar was added dynamically using Javascript showing the correct date and time.',
+    Description: 'This is a basic website that allows you to add and remove books. Styling has been added to match the provided template using Bootstrap. Classes and methods were used in this project instead of objects and arrays. A functional Navbar, footer, and contact page were created. Each nav-item contains an onclick event listener containing a function that enables the user to switch to the corresponding page. Lastly, the timestamp below the navbar was added dynamically using JavaScript, showing the correct date and time.',
     thumbnail: './Images/card_images/awesome-books.png',
     ProjectLink: 'https://keebot-png.github.io/Awesome-Books-1/',
     GitHubLink: 'https://github.com/keebot-png/Awesome-Books-1',
@@ -101,8 +101,8 @@ function modalSection(data) {
                     </ul>
                 </div>
                 <div class="buttons">
-                    <button class="modal-11" onclick="window.location.href='${popData[data].ProjectLink}'">See live</button>
-                    <button class="modal-12" onclick="window.location.href='${popData[data].GitHubLink}'">See Source</button>
+                <button type='button' class="modal-11"><a href='${popData[data].ProjectLink}' target="_blank">See live</a></button>
+                <button type='button' class="modal-12"><a href='${popData[data].GitHubLink}' target="_blank">See Source</a></button>
                 </div>
             </div>
             
@@ -110,8 +110,8 @@ function modalSection(data) {
                 <p>${popData[data].Description}</p>
             </div>
             <div class="buttons-1">
-                <button class="modal-11" onclick="window.location.href='${popData[data].ProjectLink}'">See live</button>
-                <button class="modal-12" onclick="window.location.href='${popData[data].GitHubLink}'">See Source</button>
+                <button type='button' class="modal-11"><a href='${popData[data].ProjectLink}' target="_blank">See live</a></button>
+                <button type='button' class="modal-12"><a href='${popData[data].GitHubLink}' target="_blank">See Source</a></button>
             </div>
         </div>
     </div>
@@ -145,7 +145,7 @@ const workProfile = document.querySelector('.work-profiles-10');
 for (let i = 0; i < 3; i += 1) {
   workProfile.innerHTML += `
   <div class="work-profile-1">
-  <img class="work-img" src=${popData[i].thumbnail} />
+  <img class="work-img" style="${i === 2 ? 'width: ' : ''}" src=${popData[i].thumbnail} />
   <div class="works-text">
       <div class="works-title">
           <h3>
@@ -172,7 +172,7 @@ const workProfile2 = document.querySelector('.work-profiles-20');
 for (let i = 3; i < popData.length; i += 1) {
   workProfile2.innerHTML += `
   <div class="work-profile-1">
-  <img class="work-img" src=${popData[i].thumbnail} />
+  <img class="work-img" style="${i === 4 ? 'width: ' : ''}" src=${popData[i].thumbnail} />
   <div class="works-text">
       <div class="works-title">
           <h3>
